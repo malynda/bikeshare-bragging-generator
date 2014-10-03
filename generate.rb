54e7f4a2daa6250a59c12c17e7805e3d7ae6a6fc
alexsoble <asoble@gmail.com>
2014-10-03 10:12:13 -0300

    Heh, not systematically erasing .git each time would be a good thing

--- generate.rb
+++ generate.rb
@@ -11,30 +11,30 @@ url = gets.chomp
 puts "Got it, the URL is #{url}."
 
 require 'fileutils'
-safe_list = [".", "..", "generate.rb", "template", "README.md"]
+require 'pathname'
 
-# Clear out existing generated files
-Dir.foreach(Dir.pwd) do |entry|
-   unless safe_list.include? entry
-     FileUtils.rm_r(entry)
-   end 
-end
+generated = Pathname.new("/generated")
+if generated.exist?
+  FileUtils.rm_r("generated")
+end 
 
 manifest = File.read("template/manifest_template.json")
   .gsub("#NAME#", name)
   .gsub("#VERSION#", version)
   .gsub("#URL#", url)
 
+Dir.mkdir("generated")
+Dir.chdir("generated")
 Dir.mkdir(name)
 FileUtils.cp [
-  "template/star_icon_white.png", 
-  "template/twitter_logo_white.png", 
-  "template/jquery.js", 
-  "template/jquery.livequery.js", 
-  "template/highcharts.js", 
-  "template/icon48.png", 
-  "template/icon128.png", 
-  "template/ajax-loader.gif"
+  "../template/star_icon_white.png", 
+  "../template/twitter_logo_white.png", 
+  "../template/jquery.js", 
+  "../template/jquery.livequery.js", 
+  "../template/highcharts.js", 
+  "../template/icon48.png", 
+  "../template/icon128.png", 
+  "../template/ajax-loader.gif"
 ], name
 Dir.chdir(name)
 File.open("manifest.json", "w") { |f| f.write(manifest) }
